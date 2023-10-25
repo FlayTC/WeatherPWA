08f836e4c53a0210ab0d5b616ac48d3330ad4ea5
FlayTC <flaviaalvarez2896@gmail.com>
2023-10-24 21:10:05 -0300

    Toques Fianles

--- app.js
+++ app.js
@@ -16,22 +16,22 @@ window.addEventListener("beforeinstallprompt", event => {
     console.log("beforeinstallprompt se disparó correctamente");
 });
 
-document.querySelector("#instalacion button").addEventListener("click", async (event) => {
-    if (bip) {
-        bip.prompt();
-        const { outcome } = await bip.userChoice;
-        // La variable bip solo se debe usar una vez.
-        bip = null;
-        // Actúa según la elección del usuario
-        if (outcome === 'accepted') {
-            console.log('El usuario aceptó el prompt de instalación.');
-        } else if (outcome === 'dismissed') {
-            console.log('El usuario rechazó el prompt de instalación');
-        }
-    } else {
-        alert("Se isntalo correctamente");
-    }
-});
+// document.querySelector("#instalacion button").addEventListener("click", async (event) => {
+//     if (bip) {
+//         bip.prompt();
+//         const { outcome } = await bip.userChoice;
+//         // La variable bip solo se debe usar una vez.
+//         bip = null;
+//         // Actúa según la elección del usuario
+//         if (outcome === 'accepted') {
+//             console.log('El usuario aceptó el prompt de instalación.');
+//         } else if (outcome === 'dismissed') {
+//             console.log('El usuario rechazó el prompt de instalación');
+//         }
+//     } else {
+//         alert("Se isntalo correctamente"); // Aquí se muestra el mensaje
+//     }
+// });
 
 const showDataButton = document.getElementById('showDataButton');
 const sunsData = document.getElementById('sunsData');
@@ -51,3 +51,40 @@ if (window.matchMedia('(display-mode: standalone)').matches) {
     // Ajusta el tamaño de la ventana a 800x600 píxeles
     window.resizeTo(900, 800);
 }
+
+// Esta variable almacenará el evento para su uso posterior.
+let deferredPrompt;
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Evita que aparezca el diálogo de instalación predeterminado del navegador.
+  e.preventDefault();
+  // Guarda el evento porque lo necesitarás para desencadenarlo más tarde.
+  deferredPrompt = e;
+  // Muestra tu propio diálogo de instalación personalizado.
+  showCustomInstallDialog();
+});
+
+// Función para mostrar el diálogo de instalación personalizado
+function showCustomInstallDialog() {
+  // Aquí puedes diseñar y mostrar tu propio diálogo de instalación personalizado.
+  const installDialog = document.getElementById('custom-install-dialog');
+  installDialog.style.display = 'block';
+
+  // Agrega botones, texto y elementos de diseño personalizados a tu diálogo.
+  // Puedes escuchar eventos en tus botones para manejar la instalación.
+}
+
+// Manejar la instalación cuando el usuario hace clic en un botón en el diálogo personalizado
+document.getElementById('install-button').addEventListener('click', () => {
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('El usuario aceptó la instalación');
+      }
+      // Limpia la variable deferredPrompt
+      deferredPrompt = null;
+      // Cierra el diálogo personalizado
+      document.getElementById('custom-install-dialog').style.display = 'none';
+    });
+  }
+});
